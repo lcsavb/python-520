5c0a3ea328ceafb2d24ec4366c21b1eb87c1f2c9
lcsavb <lcsavb@gmail.com>
2019-04-14 14:17:11 -0300

    Saga do gato gordo terminada.

--- estudo/sagagatogordo.py
+++ estudo/sagagatogordo.py
@@ -1,12 +1,12 @@
-import datetime
-
 lista_de_gatos = [
     {
         'nome': 'Malawi',
+        'idade': 3,
         'peso': 3750
     },
     {
         'nome': 'Gato',
+        'idade': 6,
         'peso': 6000
     }
 ]
@@ -20,25 +20,17 @@ def cadastrar_gatos():
         'nome': input('Digite o nome: \n'),
 
         'idade': conserto_erros_do_cadastro(
-            'Digite a idade do gato: ', 'Idade cadastrada com sucesso!', 'Idade inválida, tente novamente)'),
+            'Digite a idade do gato: \n', 'Idade cadastrada com sucesso!', 'Idade inválida, tente novamente!)'),
         'peso': conserto_erros_do_cadastro(
-            'Digite o peso do gato: ', 'Peso cadastrado com sucesso!', 'Peso inválido, tente novamente)')
+            'Digite o peso do gato: \n', 'Peso cadastrado com sucesso!', 'Peso inválido, tente novamente!)')
     }
     return gato
 
 
-# def change_to_int(mensagem_para_cadastro, mensagem_de_erro):
-# 	entrada_usuario = input(mensagem_para_cadastro)
-# 	try:
-# 		return int(entrada_usuario)
-# 	except ValueError:
-# 		raise ValueError(mensagem_de_erro)
-
-
 def conserto_erros_do_cadastro(mensagem_para_cadastro, mensagem_de_sucesso, mensagem_de_erro):
-    while not False:
+    while True:
         try:
-            entrada_do_usuario_bozo = int(input(mensagem_para_cadastro))
+            entrada_do_usuario_bozo = checar_se_inteiro(mensagem_para_cadastro)
             print(mensagem_de_sucesso)
             break
         except ValueError:
@@ -46,16 +38,39 @@ def conserto_erros_do_cadastro(mensagem_para_cadastro, mensagem_de_sucesso, mens
     return entrada_do_usuario_bozo
 
 
+def checar_se_inteiro(mensagem_para_cadastro):
+    try:
+        entrada_usuario = int(input(mensagem_para_cadastro))
+    except ValueError:
+        raise
+    return entrada_usuario
+
+
 for gato in range(1):
     lista_de_gatos.append(cadastrar_gatos())
 
-print(lista_de_gatos)
+
+TEMPLATE = '{:>16} ||| {:>16} ||| {:>16}'
+
+
+CABECALHO = TEMPLATE.format('Nome do gatinho', 'Idade', 'Peso')
+
+
+print(CABECALHO)
+for gato in lista_de_gatos:
+    gato_ajustado = TEMPLATE.format(
+        gato['nome'],
+        gato['idade'],
+        gato['peso']
+    )
+    print(gato_ajustado)
+
 
 for gato in lista_de_gatos:
     if gato['peso'] > 5000:
         gatos_gordos.append(gato)
-else:
-    gatos_magros.append(gato)
+    else:
+        gatos_magros.append(gato)
 
 
 print('Esses são os gatos magros:')
@@ -64,3 +79,4 @@ print(gatos_magros)
 
 print('Esses são os gatos gordos:')
 print(gatos_gordos)
+
